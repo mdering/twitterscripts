7bac06c996ab46353f6ded4f8bae302ddd30dff1
Matt Dering <matthew.dering@gmail.com>
2015-05-29 18:53:03 -0400

    downloader fix

--- downloads_from_jsons.py
+++ downloads_from_jsons.py
@@ -8,20 +8,26 @@ import MySQLdb
 from tokens import *
 
 
-f = open("tweets.csv")
+f = open("tweets.txt")
 
 images = []
 urls = []
 for line in f:
 	tweet=line.strip().split("|")
-	js = tweet[1]
+	if len(tweet)>1:
+		js = '|'.join(tweet[1:])
+	else:
+		continue
 	data=json.loads(js)
-	for pic in data['entities']['media']:
-		#print data['id_str'],pic['media_url']
+	if 'entities' in data:
+		if 'media' in data['entites']:
+			for pic in data['entities']['media']:
+				#print data['id_str'],pic['media_url']
 
-		print data['id_str'],pic['media_url']
-		urls.append(pic['media_url'])
+				print data['id_str'],pic['media_url']
+				urls.append(pic['media_url'])
 
+rowno=0
 for url in urls:
 	rowno+=1
 	filename = url.split("/")[-1]
